f3c24ee5442fd4491522a2290df172f6c3ea6363
hxy-pc <oheasy1990@163.com>
2015-03-20 00:07:43 +0800

    3/20

--- src/main/java/hxy/com/myapplication/MainActivity.java
+++ src/main/java/hxy/com/myapplication/MainActivity.java
@@ -1,39 +1,61 @@
 package hxy.com.myapplication;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 
 
 public class MainActivity extends Activity {
-
+    View view = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        view = findViewById(R.id.linearlayout);
+        AlphaAnimation animation = new AlphaAnimation(0.03f, 1.0f);
+        animation.setDuration(2000);
+        view.setAnimation(animation);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                view.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                try {
+//                    Thread.sleep(2000);
+//                    Intent intent = new Intent(MainActivity.this, TabHostActivity.class);
+//                    startActivity(intent);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }).start();
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onStop() {
+        super.onStop();
+        finish();
     }
 }
---
+++ src/main/java/hxy/com/myapplication/TabHostActivity.java
@@ -0,0 +1,15 @@
+package hxy.com.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by hxy-pc on 2015/3/19.
+ */
+public class TabHostActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
